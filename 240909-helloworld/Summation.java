5deb485dd29e721103d84b72961c696e65898cc7
azrubael <i0638464000@gmail.com>
2024-09-09 21:36:15 +0300

    Updated -- 240909-helloworld/Summation.java, after testing.

--- 240909-helloworld/Summation.java
+++ 240909-helloworld/Summation.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Summation {
     public static void main(String[] args) {
+        String explanation = "Please enter a whole number greater zero: ";
+        System.out.println(explanation);
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        int num;
+        do {
+            while (!sc.hasNextInt()) {
+                sc.next();
+                System.out.println(explanation);
+            }
+            num = sc.nextInt();
+            if (num <= 0) {
+                System.out.println(explanation);
+            }
+        } while (num <= 0);
+
         int sum = 0;
         int count = 0;
         while (count <= num) {
